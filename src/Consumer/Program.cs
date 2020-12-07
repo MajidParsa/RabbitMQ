562753cc67391d288cee0c5cf3f7101941399f94
Majid Parsa <MajidKahaki@gmail.com>
2020-12-07 09:40:46 +0330

    Add project files.

---
+++ src/Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("*** Consumer ***");
+
+			var factory = new ConnectionFactory()
+			{
+				HostName = "localhost",
+				//UserName = "Producer",
+				//Password = "Pr@ducer"
+			};
+
+			using var connection = factory.CreateConnection();
+			using var channel = connection.CreateModel();
+
+			channel.QueueDeclare(queue: "default", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+			var consumer = new EventingBasicConsumer(channel);
+			consumer.Received += Consumer_Received;
+
+			channel.BasicConsume(queue: "default", autoAck: true, consumer: consumer);
+			Console.ReadLine();
+		}
+
+		private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+		{
+			var message = Encoding.UTF8.GetString(e.Body.ToArray());
+			Console.WriteLine(message);
+		}
+	}
+}
---
+++ src/Producer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("*** Producer ***");
+
+			var factory = new ConnectionFactory()
+			{
+				HostName = "localhost", 
+				//UserName = "Producer",
+				//Password = "Pr@ducer"
+			};
+
+			using var connection = factory.CreateConnection();
+			using var channel = connection.CreateModel();
+
+			channel.QueueDeclare(queue:"default", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+			var message = "Hello world ;)";
+			while (!string.IsNullOrWhiteSpace(message))
+			{
+				Console.WriteLine("Enter a Message: ");
+				message = Console.ReadLine();
+
+				var body = Encoding.UTF8.GetBytes(message);
+				channel.BasicPublish(exchange: "", routingKey: "default", basicProperties: null, body: body);
+
+				Console.WriteLine("Message send from [Produser] to [Consumer]: " + message);
+
+			}
+		}
+	}
+}
